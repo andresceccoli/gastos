557a559f7385099cd7b465eadf856095dcd7939e
andres.ceccoli@waveapps.com.ar <Satoshi96>
2017-10-10 18:12:46 -0300

    commit inicial

---
+++ app/src/main/java/com/androidutn/gastos/model/Resumen.java
@@ -0,0 +1,27 @@
+package com.androidutn.gastos.model;
+
+/**
+ * Created by andres on 9/27/17.
+ */
+
+public class Resumen {
+
+    private double totalIngresos;
+    private double totalEgresos;
+
+    public double getTotalIngresos() {
+        return totalIngresos;
+    }
+
+    public void setTotalIngresos(double totalIngresos) {
+        this.totalIngresos = totalIngresos;
+    }
+
+    public double getTotalEgresos() {
+        return totalEgresos;
+    }
+
+    public void setTotalEgresos(double totalEgresos) {
+        this.totalEgresos = totalEgresos;
+    }
+}
