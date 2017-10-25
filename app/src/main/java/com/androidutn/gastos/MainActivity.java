fc8ca1f30dc4ad27f19d3005a5e96015a043ca67
andres.ceccoli@waveapps.com.ar <andres.ceccoli@gmail.com>
2017-10-24 22:18:59 -0300

    Clase 3

--- app/src/main/java/com/androidutn/gastos/MainActivity.java
+++ app/src/main/java/com/androidutn/gastos/MainActivity.java
@@ -2,23 +2,34 @@ package com.androidutn.gastos;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.androidutn.gastos.model.Resumen;
+import com.androidutn.gastos.model.ResumenCategoria;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.PieChartView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.text_egresos) TextView mTextEgresos;
     @BindView(R.id.text_saldo) TextView mTextSaldo;
 
+    @BindView(R.id.grafico_egresos) PieChartView mGraficoEgresos;
+    @BindView(R.id.grafico_ingresos) PieChartView mGraficoIngresos;
+
     private Calendar mesActual;
 
     @Override
@@ -43,9 +57,92 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        mGraficoEgresos.setCircleFillRatio(0.7f);
+        mGraficoIngresos.setCircleFillRatio(0.7f);
+
         mesActual = Calendar.getInstance();
         mostrarMes();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        cargarInfo();
+    }
+
+    private void cargarInfo() {
+        FirebaseDatabase.getInstance().getReference("resumen")
+                .child(MES_DB_FORMAT.format(mesActual.getTime()))
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Resumen resumen = dataSnapshot.getValue(Resumen.class);
+
+                        if (resumen != null) {
+                            mTextIngresos.setText(
+                                    NumberFormat.getCurrencyInstance().format(
+                                            resumen.getTotalIngresos()));
+
+                            mTextEgresos.setText(
+                                    NumberFormat.getCurrencyInstance().format(
+                                            resumen.getTotalEgresos()));
+
+                            mTextSaldo.setText(NumberFormat.getCurrencyInstance().format(
+                                    resumen.getTotalIngresos() - resumen.getTotalEgresos()));
+                        } else {
+                            mTextIngresos.setText(null);
+                            mTextEgresos.setText(null);
+                            mTextSaldo.setText(null);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+
+        FirebaseDatabase.getInstance().getReference("resumen_egresos")
+                .child(MES_DB_FORMAT.format(mesActual.getTime()))
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        mostrarGrafico(mGraficoEgresos, dataSnapshot);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {}
+                });
+        FirebaseDatabase.getInstance().getReference("resumen_ingresos")
+                .child(MES_DB_FORMAT.format(mesActual.getTime()))
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        mostrarGrafico(mGraficoIngresos, dataSnapshot);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {}
+                });
+    }
+
+    private void mostrarGrafico(PieChartView grafico, DataSnapshot data) {
+        List<SliceValue> values = new ArrayList<>();
+
+        for (DataSnapshot ds : data.getChildren()) {
+            ResumenCategoria resumenCat = ds.getValue(ResumenCategoria.class);
+            SliceValue slice = new SliceValue((float) resumenCat.getTotal(), ChartUtils.nextColor());
+            slice.setLabel(resumenCat.getNombre());
+            values.add(slice);
+        }
+
+        PieChartData pieChartData = new PieChartData(values);
+        pieChartData.setHasLabels(true)
+                .setHasLabelsOutside(true)
+                .setHasLabelsOnlyForSelected(false);
+        grafico.setPieChartData(pieChartData);
     }
 
     private void mostrarMes() {
@@ -56,12 +153,14 @@ public class MainActivity extends AppCompatActivity {
     public void onMesAnterior() {
         mesActual.add(Calendar.MONTH, -1);
         mostrarMes();
+        cargarInfo();
     }
 
     @OnClick(R.id.mes_siguiente)
     public void onMesSiguiente() {
         mesActual.add(Calendar.MONTH, 1);
         mostrarMes();
+        cargarInfo();
     }
 
     @OnClick(R.id.nuevo_ingreso)
--- app/src/main/java/com/androidutn/gastos/MovimientoActivity.java
+++ app/src/main/java/com/androidutn/gastos/MovimientoActivity.java
@@ -9,15 +9,20 @@ import android.widget.Spinner;
 
 import com.androidutn.gastos.model.Categoria;
 import com.androidutn.gastos.model.Movimiento;
+import com.androidutn.gastos.model.Resumen;
+import com.androidutn.gastos.model.ResumenCategoria;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,6 +33,8 @@ import io.blackbox_vision.datetimepickeredittext.view.TimePickerInputEditText;
 public class MovimientoActivity extends AppCompatActivity {
 
     public static final String EXTRA_INGRESO = "movIngreso";
+
+    private static final SimpleDateFormat MES_DB_FORMAT = new SimpleDateFormat("yyyyMM", Locale.getDefault());
 
     @BindView(R.id.mov_categoria) Spinner mCategoria;
     @BindView(R.id.mov_monto) TextInputLayout mMonto;
@@ -49,8 +56,14 @@ public class MovimientoActivity extends AppCompatActivity {
         mFechaInput.setManager(getSupportFragmentManager());
         mHoraInput.setManager(getSupportFragmentManager());
 
-        mFechaInput.setDate(Calendar.getInstance());
-        mHoraInput.setTime(Calendar.getInstance());
+        Calendar c = Calendar.getInstance();
+        mFechaInput.onDateSet(null,
+                c.get(Calendar.YEAR),
+                c.get(Calendar.MONTH),
+                c.get(Calendar.DATE));
+        mHoraInput.onTimeSet(null,
+                c.get(Calendar.HOUR_OF_DAY),
+                c.get(Calendar.MINUTE));
 
         ingreso = getIntent().getBooleanExtra(EXTRA_INGRESO, false);
 
@@ -107,7 +120,7 @@ public class MovimientoActivity extends AppCompatActivity {
             mMonto.setErrorEnabled(false);
         }
 
-        Movimiento mov = new Movimiento();
+        final Movimiento mov = new Movimiento();
         mov.setIngreso(ingreso);
         mov.setMonto(Double.parseDouble(mMonto.getEditText().getText().toString()));
         mov.setDescripcion(mDescripcion.getEditText().getText().toString());
@@ -126,7 +139,81 @@ public class MovimientoActivity extends AppCompatActivity {
 
         DatabaseReference movimientos = FirebaseDatabase.getInstance().getReference("movimientos");
         String key = movimientos.push().getKey();
-        movimientos.child(key).setValue(mov);
+        movimientos.child(key).setValue(mov, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                if (databaseError == null) {
+                    actualizarResumen(mov);
+                }
+            }
+        });
+    }
+
+    private void actualizarResumen(final Movimiento mov) {
+        FirebaseDatabase.getInstance().getReference("resumen")
+                .child(MES_DB_FORMAT.format(new Date(mov.getFecha())))
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Resumen resumen = dataSnapshot.getValue(Resumen.class);
+                        if (resumen == null) {
+                            resumen = new Resumen();
+                        }
+
+                        if (ingreso) {
+                            resumen.setTotalIngresos(resumen.getTotalIngresos() + mov.getMonto());
+                        } else {
+                            resumen.setTotalEgresos(resumen.getTotalEgresos() + mov.getMonto());
+                        }
+
+                        dataSnapshot.getRef().setValue(resumen, new DatabaseReference.CompletionListener() {
+                            @Override
+                            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                                if (databaseError == null) {
+                                    actualizarResumenCategoria(mov);
+                                }
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    private void actualizarResumenCategoria(final Movimiento mov) {
+        FirebaseDatabase.getInstance().getReference(
+                ingreso ? "resumen_ingresos" : "resumen_egresos")
+                .child(MES_DB_FORMAT.format(new Date(mov.getFecha())))
+                .child(mov.getCategoriaKey())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        ResumenCategoria resumenCat = dataSnapshot.getValue(ResumenCategoria.class);
+                        if (resumenCat == null) {
+                            resumenCat = new ResumenCategoria();
+                        }
+
+                        resumenCat.setNombre(mov.getCategoriaNombre());
+                        resumenCat.setTotal(resumenCat.getTotal() + mov.getMonto());
+
+                        dataSnapshot.getRef().setValue(resumenCat, new DatabaseReference.CompletionListener() {
+                            @Override
+                            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                                if (databaseError == null) {
+                                    finish();
+                                }
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     @OnClick(R.id.mov_nueva_categoria)
